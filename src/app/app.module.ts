9f6278322cf183d4527f62924c070e27fde46471
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-08-21 11:54:20 -0500

    Servicio para buscar por países

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -14,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     PaisModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,7 +9,13 @@ import { Component } from '@angular/core';
 export class PorPaisComponent {
   termino: string = '';
 
+  constructor(private paisService: PaisService) {}
+
   buscar() {
     console.log(this.termino);
+    this.paisService.buscarPais(this.termino)
+    .subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
