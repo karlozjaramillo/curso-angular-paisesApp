238aed67a91b2494fb4e44bfcb1b07e8a96b36dc
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-08-23 08:18:15 -0500

    Componente Input y Tabla

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [],
+})
+export class PaisInputComponent {
+  termino: string = '';
+
+  buscar() {
+    console.log('Hola Mundo');
+    console.log(this.termino);
+  }
+}
