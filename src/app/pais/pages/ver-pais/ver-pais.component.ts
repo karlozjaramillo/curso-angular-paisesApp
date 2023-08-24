ea95613e3ba0ea368b0cb25db00f9a9cf8f4227b
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-08-24 08:45:05 -0500

    Terminar la pantalla de ver país

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,18 +10,20 @@ import { switchMap } from 'rxjs/operators';
   styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country[];
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
   ) {}
+
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id))
-        )
-      .subscribe((resp) => {
-        console.log(resp);
-      });
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => (this.pais = pais));
 
     // this.activatedRoute.params
     // .subscribe(({id}) => {
