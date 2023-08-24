e23098ac7c53cd529f2cb952bee983bd0892ff54
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2023-08-24 10:16:19 -0500

    ngClass, class y [class.]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
+  activarRegion(region: string) {
+    this.regionActiva = region;
+
+    //TODO: hacer el llamado al servicio
+  }
 }
